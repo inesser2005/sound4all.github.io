265e6bab193ac609f061d5460cfb2e73b05fb32d
Inês Sérgio <inesser2005@gmail.com>
2026-05-11 20:36:37 +0100

    menu update

--- my-app/src/comp/AppNavbar.jsx
+++ my-app/src/comp/AppNavbar.jsx
@@ -17,13 +17,13 @@ export default function AppNavbar() {
     return (
         <nav className="navbar navbar-dark menu_bg_color fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Menu</a>
+                <p className="navbar-brand" >Sound4all</p>
                 <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="offcanvas offcanvas-end text-bg-dark" tabIndex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
                     <div className="offcanvas-header">
-                        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+                        <h5 className="offcanvas-title" id="offcanvasDarkNavbarLabel">Sound4all</h5>
                         <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                     </div>
                     {/* Linking das páginas*/}
